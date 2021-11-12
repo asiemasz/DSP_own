d24024ebe60e8c9f2e3186b4eb713a2359608b5f
Adam Siemasz <asiemasz1@gmail.com>
2021-11-12 18:54:02 +0100

    fix - SPB instead of symbolSpan in equations

--- src/SRRC_filter.c
+++ src/SRRC_filter.c
@@ -11,17 +11,17 @@ void SRRC_getFIRCoeffs(uint8_t symbolSpan, uint8_t SPB, float32_t beta, float32_
     assert(symbolSpan * SPB % 2 == 0);
 
     float32_t presc;
-    arm_sqrt_f32(symbolSpan, &presc);
+    arm_sqrt_f32(SPB, &presc);
 
     for( uint8_t i = 0; i < length; i++) {
         int16_t n = i - length/2;
         if(n == 0) {
             *(firCoeffs + i) = (1 / presc) * (1 - beta + 4*beta/PI);
         }
-        else if(n == symbolSpan/4/beta || n == -symbolSpan/4/beta) {
+        else if(n == SPB/4/beta || n == -SPB/4/beta) {
             *(firCoeffs + i) = beta/presc/1.4142f * ((1 + 2/PI) * sin(PI/4/beta) + (1 - 2/PI) * cos(PI/4/beta));
         }
         else 
-            *(firCoeffs + i) = (sin(PI*n*(1 - beta)/symbolSpan) + 4*beta*n/symbolSpan*cos(PI*n*(1+beta)/symbolSpan)) / (PI*n/symbolSpan * (1 - 16*beta*beta*n*n/(symbolSpan*symbolSpan))) / presc;
+            *(firCoeffs + i) = (sin(PI*n*(1 - beta)/SPB) + 4*beta*n/SPB*cos(PI*n*(1+beta)/SPB)) / (PI*n/SPB * (1 - 16*beta*beta*n*n/(SPB*SPB))) / presc;
     }
 }
