799ac3932790821bca5f8ae412fce649194a6fcd
Adam Siemasz <asiemasz1@gmail.com>
2021-11-15 19:45:14 +0100

    Change function names

--- src/BPSK.c
+++ src/BPSK.c
@@ -1,7 +1,7 @@
 #include "BPSK.h"
 
 //turn bytes into -1 (corresponding to 0) and 1 (corresponding to 1) array
-static void generateModData(uint8_t* data, uint16_t length, int8_t* output) {
+static void BPSK_generateModData(uint8_t* data, uint16_t length, int8_t* output) {
     for(uint16_t i = 0; i < length; i++) {
         for(uint8_t j = 0; j < 8; j++) {
             *(output + i*8 + j) = (data[i] & (0x80 >> j)) ? -1 : 1;
@@ -10,7 +10,7 @@ static void generateModData(uint8_t* data, uint16_t length, int8_t* output) {
 }
 
 //generate modulation samples (with oversampling)
-void getModSamples(BPSK_parameters* params, uint8_t* data, uint16_t length, float32_t* outData, uint16_t outLength) {
+void BPSK_getModSamples(BPSK_parameters* params, uint8_t* data, uint16_t length, float32_t* outData, uint16_t outLength) {
     uint8_t samplesPerBit = params->Fs / params->Fb; //calculate samples per bit parameter (Fs should be multiply of Fb)
 
     assert(samplesPerBit > 0);
