de5724937bd3173cf724149a53f894368250e340
Adam Siemasz <asiemasz1@gmail.com>
2021-11-16 22:45:30 +0100

    Add demodulation function

--- src/BPSK.c
+++ src/BPSK.c
@@ -26,9 +26,9 @@ void BPSK_getModSamples(BPSK_parameters* params, uint8_t* data, uint16_t length,
     }
 
     if(params->firCoeffsLength) {
-        float32_t tempData[outLength + params->firCoeffsLength - 1];
+        float32_t tempData[outLength];
 
-        arm_conv_f32(outData, outLength, params->firCoeffs, params->firCoeffsLength, tempData);
+        arm_conv_partial_f32(outData, outLength, params->firCoeffs, params->firCoeffsLength, tempData, 0, outLength);
         float32_t maxVal;
         void* x;
         arm_max_f32(tempData, outLength, &maxVal, x);
@@ -51,5 +51,28 @@ void BPSK_getOutputSignal(BPSK_parameters* params, uint8_t* data, uint16_t dataL
 
     for(uint16_t i = 0; i < outLength; i++) {
         outSignal[i] = outSignal[i] * arm_sin_f32(fn*i*2*PI);
+    }
+}
+
+void BPSK_demodulateSignal(BPSK_parameters* params, float32_t* signal, uint16_t signalLength, uint8_t* outData, uint16_t outLength) {
+    uint8_t samplesPerBit = params->Fs / params->Fb; //calculate samples per bit parameter (Fs should be multiply of Fb)
+    float32_t fn = (float32_t)params->Fc / params-> Fs;
+
+    for(uint16_t i = 0; i < signalLength; i++) {
+        signal[i] = signal[i] * arm_sin_f32(fn*i*2*PI);
+    }
+
+    float32_t outSignal[signalLength];
+
+    arm_conv_partial_f32(signal, signalLength, params->firCoeffs, params->firCoeffsLength, outSignal, 0, signalLength);
+
+    uint16_t k = 0;
+
+    for(uint16_t i = samplesPerBit - 1; i < signalLength; i = i + samplesPerBit*8) { //init version 
+        outData[k] = 0;
+        for(uint16_t j = 0; j < 8*samplesPerBit; j = j + samplesPerBit) {
+            outData[k] += outSignal[i+j] < 0 ? (1 << (7 - j/samplesPerBit)) : 0;
+        }
+        ++k;
     }
 }
--- src/BPSK.h
+++ src/BPSK.h
@@ -14,4 +14,7 @@ typedef struct BPSK_parameters {
 void BPSK_getModSamples(BPSK_parameters* params, uint8_t* data, uint16_t length, float32_t* outData, uint16_t outLength);
 
 void BPSK_getOutputSignal(BPSK_parameters* params, uint8_t* data, uint16_t dataLength, float32_t* outSignal, uint16_t outLength);
+
+void BPSK_demodulateSignal(BPSK_parameters* params, float32_t* signal, uint16_t signalLength, uint8_t* outData, uint16_t outLength);
+
 #endif
