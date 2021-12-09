90f5b37cf68c84444f41e42b70b23639329b3df4
asiemasz <asiemasz1@gmail.com>
2021-12-09 18:29:46 +0100

    auto formattting

--- src/BPSK.h
+++ src/BPSK.h
@@ -1,25 +1,32 @@
 #ifndef BPSK_H
 #define BPSK_H
-#include <stdint.h>
 #include <arm_math.h>
+#include <stdint.h>
 
 typedef struct BPSK_parameters {
-    uint16_t Fc; // carrier frequency
-    uint32_t Fs; // sampling frequency
-    uint16_t Fb; // bit rate (bps)
-    uint16_t FSpan; //filter span in symbols
-    uint16_t prefixLength; //prefix length (for synchro purposes)
-    uint16_t frameLength; //one data frame length
-    float32_t* firCoeffs;
-    uint16_t firCoeffsLength;
+  uint16_t Fc;           // carrier frequency
+  uint32_t Fs;           // sampling frequency
+  uint16_t Fb;           // bit rate (bps)
+  uint16_t FSpan;        // filter span in symbols
+  uint16_t prefixLength; // prefix length (for synchro purposes)
+  uint16_t frameLength;  // one data frame length
+  float32_t *firCoeffs;
+  uint16_t firCoeffsLength;
 } BPSK_parameters;
 
-void BPSK_getModSamples(BPSK_parameters* params, uint8_t* data, uint16_t length, float32_t* outData, uint16_t outLength);
+void BPSK_getModSamples(BPSK_parameters *params, uint8_t *data, uint16_t length,
+                        float32_t *outData, uint16_t outLength);
 
-void BPSK_getOutputSignal(BPSK_parameters* params, uint8_t* data, uint16_t dataLength, float32_t* outSignal, uint16_t outLength);
+void BPSK_getOutputSignal(BPSK_parameters *params, uint8_t *data,
+                          uint16_t dataLength, float32_t *outSignal,
+                          uint16_t outLength);
 
-void BPSK_demodulateSignal(BPSK_parameters* params, float32_t* signal, uint16_t signalLength, uint8_t* outData, uint16_t outLength);
+void BPSK_demodulateSignal(BPSK_parameters *params, float32_t *signal,
+                           uint16_t signalLength, uint8_t *outData,
+                           uint16_t outLength);
 
-void BPSK_syncInputSignal(BPSK_parameters* params, float32_t* signal, uint16_t signalLength, uint32_t* startIdx);
+void BPSK_syncInputSignal(BPSK_parameters *params, float32_t *signal,
+                          uint16_t signalLength, uint32_t *startIdx,
+                          uint16_t *foundIdx);
 
 #endif
