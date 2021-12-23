8accb93756aab7bfa7afa1bc7fe5fe9204648c13
asiemasz <asiemasz1@gmail.com>
2021-12-23 21:45:38 +0100

    Add simple IIR filters implementation using bare C

---
+++ src/IIR_filter.c
@@ -0,0 +1,53 @@
+#include "IIR_filter.h"
+
+IIR_filter IIR_filter_init(float32_t *coeffs_A, uint16_t length_A,
+                           float32_t *coeffs_B, uint16_t length_B) {
+  IIR_filter ret;
+  ret.coeffs_A = coeffs_A;
+  ret.coeffs_B = coeffs_B;
+  ret.n_A = length_A - 1;
+  ret.n_B = length_B - 1;
+
+  double buf_B[length_B - 1];
+  double buf_A[length_A - 1];
+
+  ret.buf_B = buf_B;
+  ret.buf_A = buf_A;
+
+  ret.pos_B = 0;
+  ret.pos_A = 0;
+
+  return ret;
+}
+
+float32_t IIR_filter_step(IIR_filter *filter, float32_t input_value) {
+  float32_t acc = filter->coeffs_B[0] * input_value;
+
+  for (uint16_t i = 1; i <= filter->n_B; ++i) {
+    uint16_t p = (filter->pos_B + filter->n_B - i) % filter->n_B;
+    acc += filter->coeffs_B[i] * filter->buf_B[p];
+  }
+  for (uint16_t i = 1; i <= filter->n_A; ++i) {
+    uint16_t p = (filter->pos_A + filter->n_A - i) % filter->n_A;
+    acc -= filter->coeffs_A[i] * filter->buf_A[p];
+  }
+
+  if (filter->n_B > 0) {
+    filter->buf_B[filter->pos_B] = input_value;
+    filter->pos_B = (filter->pos_B + 1) % filter->n_B;
+  }
+
+  if (filter->n_A > 0) {
+    filter->buf_A[filter->pos_A] = acc;
+    filter->pos_A = (filter->pos_A + 1) % filter->n_A;
+  }
+
+  return acc;
+}
+
+float32_t IIR_filter_run(IIR_filter *filter, float32_t *input,
+                         uint16_t input_length, float32_t *output) {
+  for (uint16_t i = 0; i < input_length; i++) {
+    output[i] = IIR_filter_step(filter, input[i]);
+  }
+}
---
+++ src/IIR_filter.h
@@ -0,0 +1,25 @@
+/** Created as a port of biz library for DSP in Java (www.source-code.biz,
+www.inventec.ch/chdh) for C ARM */
+#ifndef IIR_FILTER_H
+#define IIR_FILTER_H
+
+#include <arm_math.h>
+#include <stdint.h>
+
+typedef struct {
+  float32_t *coeffs_A; // Applied to output values
+  float32_t *coeffs_B; // Aplied to input values
+  uint16_t n_A;        // Output signal delay line length
+  uint16_t n_B;        // Input signal delay line length
+  float32_t *buf_B;    // Input signal delay line
+  float32_t *buf_A;    // Output signal delay line
+  uint16_t pos_B;      // Position in input signal delay line
+  uint16_t pos_A;      // Position in output signal delay line
+} IIR_filter;
+
+IIR_filter IIR_filter_init(float32_t *coeffs_A, uint16_t length_A,
+                           float32_t *coeffs_B, uint16_t length_B);
+
+float32_t IIR_filter_step(IIR_filter *filter, float32_t input_value);
+
+#endif
