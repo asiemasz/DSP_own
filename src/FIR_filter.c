1ffb74f7be7ad358193cabaf0ece948a14d27997
asiemasz <asiemasz1@gmail.com>
2022-03-14 20:39:53 +0100

    Add simple DSP FIR filter implementation

---
+++ src/FIR_filter.c
@@ -0,0 +1,36 @@
+#include "FIR_filter.h"
+
+FIR_filter FIR_filter_init(float32_t *coeffs, uint16_t coeffs_length,
+                           float32_t *buf) {
+  FIR_filter ret;
+  ret.coeffs = coeffs;
+  ret.length = coeffs_length;
+  ret.buf = buf;
+  ret.idx = 0;
+  return ret;
+}
+
+float32_t FIR_filter_step(FIR_filter *filter, float32_t input_value) {
+  float32_t ret;
+  int16_t i, index;
+
+  filter->buf[filter->idx++] = input_value;
+  filter->idx = filter->idx % filter->length;
+  ret = 0.0f;
+
+  index = filter->idx;
+  for (i = 0; i < filter->length; i++) {
+    --index;
+    if (index < 0)
+      index = filter->length - 1;
+    ret += filter->buf[index] * filter->coeffs[i];
+  }
+  return ret;
+}
+
+float32_t FIR_filter_run(FIR_filter *filter, float32_t *input,
+                         uint16_t input_length, float32_t *output) {
+  for (uint16_t i = 0; i < input_length; i++) {
+    output[i] = FIR_filter_step(filter, input[i]);
+  }
+}
---
+++ src/FIR_filter.h
@@ -0,0 +1,22 @@
+#ifndef FIR_FILTER_H
+#define FIR_FILTER_H
+
+#include <arm_math.h>
+#include <stdint.h>
+
+typedef struct {
+  float32_t *coeffs;
+  uint16_t length;
+  float32_t *buf;
+  uint16_t idx;
+} FIR_filter;
+
+FIR_filter FIR_filter_init(float32_t *coeffs, uint16_t coeffs_length,
+                           float32_t *buf);
+
+float32_t FIR_filter_step(FIR_filter *filter, float32_t input_value);
+
+float32_t FIR_filter_run(FIR_filter *filter, float32_t *input,
+                         uint16_t input_length, float32_t *output);
+
+#endif
