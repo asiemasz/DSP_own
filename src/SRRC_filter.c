e5ceaec282d4c7fcfe8dd5f7ad73139d0966ae9a
asiemasz <asiemasz1@gmail.com>
2022-05-03 12:16:43 +0200

    Add linear gain parameter to SRRC filter

--- src/SRRC_filter.c
+++ src/SRRC_filter.c
@@ -6,22 +6,28 @@
     SPB - samples per bit
     beta - rolloff factor (usually used 0.2)
 */
-void SRRC_getFIRCoeffs(uint8_t symbolSpan, uint8_t SPB, float32_t beta, float32_t* firCoeffs, uint16_t length) {
-    assert(length == symbolSpan*SPB + 1);
-    assert(symbolSpan * SPB % 2 == 0);
+void SRRC_getFIRCoeffs(uint8_t symbolSpan, uint8_t SPB, float32_t beta,
+                       float32_t *firCoeffs, uint16_t length,
+                       float32_t linearGain) {
+  assert(length == symbolSpan * SPB + 1);
+  assert(symbolSpan * SPB % 2 == 0);
 
-    float32_t presc;
-    arm_sqrt_f32(SPB, &presc);
+  float32_t presc;
+  arm_sqrt_f32(SPB, &presc);
 
-    for( uint16_t i = 0; i < length; i++) {
-        int16_t n = i - length/2;
-        if(n == 0) {
-            *(firCoeffs + i) = (1 / presc) * (1 - beta + 4*beta/PI);
-        }
-        else if(n == SPB/(4*beta) || n == -SPB/(4*beta)) {
-            *(firCoeffs + i) = beta/(presc*1.4142f) * ((1 + 2/PI) * sin(PI/(4*beta)) + (1 - 2/PI) * cos(PI/(4*beta)));
-        }
-        else 
-            *(firCoeffs + i) = (sin(PI*n*(1 - beta)/SPB) + 4*beta*n/SPB*cos(PI*n*(1+beta)/SPB)) / (PI*n/SPB * (1 - 16*beta*beta*n*n/(SPB*SPB))) / presc;
-    }
+  for (uint16_t i = 0; i < length; i++) {
+    int16_t n = i - length / 2;
+    if (n == 0) {
+      *(firCoeffs + i) = linearGain * (1 / presc) * (1 - beta + 4 * beta / PI);
+    } else if (n == SPB / (4 * beta) || n == -SPB / (4 * beta)) {
+      *(firCoeffs + i) = linearGain * beta / (presc * 1.4142f) *
+                         ((1 + 2 / PI) * sin(PI / (4 * beta)) +
+                          (1 - 2 / PI) * cos(PI / (4 * beta)));
+    } else
+      *(firCoeffs + i) =
+          linearGain *
+          (sin(PI * n * (1 - beta) / SPB) +
+           4 * beta * n / SPB * cos(PI * n * (1 + beta) / SPB)) /
+          (PI * n / SPB * (1 - 16 * beta * beta * n * n / (SPB * SPB))) / presc;
+  }
 }
--- src/SRRC_filter.h
+++ src/SRRC_filter.h
@@ -3,6 +3,8 @@
 #include "arm_math.h"
 #include <stdint.h>
 
-void SRRC_getFIRCoeffs(uint8_t symbolSpan, uint8_t SPB, float32_t beta, float32_t* firCoeffs, uint16_t length);
+void SRRC_getFIRCoeffs(uint8_t symbolSpan, uint8_t SPB, float32_t beta,
+                       float32_t *firCoeffs, uint16_t length,
+                       float32_t linearGain);
 
 #endif
